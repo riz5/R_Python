8d8fc85e9e7b05ccb2e8902afc70c4c39edf74ab
rizwana <rizwana@rizwanasanadi1509@gmail.com>
2021-04-30 14:58:08 +0530

    first py prgm

---
+++ p1.py
@@ -0,0 +1,17 @@
+a = 10
+print(a)
+id(a)
+list1 = [1,2,3]
+print(list1)
+list3 = ['how','are','you']
+list1.append(4)
+
+list1.extend([44,55,66])
+print(list1)
+n = list1.count(list1)
+print(n)
+#print(count(list1))
+list2 = list1.reverse()
+print(list2)
+print("-".join(list3))
+print(list3)
